4a80afcef0a600296534eb23ef00d2c0e1f4dd3a
Lookandme <kgg9386@gmail.com>
2024-09-11 20:49:36 +0900

    Soundpackage

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -17,6 +17,13 @@ public class GameManager : MonoBehaviour
 
     AudioSource audioSource;
     public AudioClip clip;
+    public AudioClip MatchClip;
+    public AudioClip FailClip;
+    public AudioClip GameOverClip;
+    public AudioClip ClearClip;
+
+
+
 
     public int cardCount = 0;
     float time = 0.0f;
@@ -38,30 +45,37 @@ public class GameManager : MonoBehaviour
         timeTxt.text = time.ToString("N2");
         if (time >= 30.0f)
         {
+            audioSource.PlayOneShot(GameOverClip);
+            Debug.Log("끝났어요");
             timeTxt.text = "30.0";
             Time.timeScale = 0.0f;
-            Fail.SetActive(true); 
+            Fail.SetActive(true);
         }
     }
-    
+
     public void Matched()
     {
-        if(firstCard.idx == secondCard.idx)
+        if (firstCard.idx == secondCard.idx)
         {
-            audioSource.PlayOneShot(clip);
+            audioSource.PlayOneShot(MatchClip);
+            Debug.Log("잘했어요");
             MatchingImage.SetActive(true);
             Invoke("CloseMatchingImage", 1f);
             firstCard.DestoryCard();
             secondCard.DestoryCard();
             cardCount -= 2;
-            if(cardCount == 0 )
+            if (cardCount == 0)
             {
+                audioSource.PlayOneShot(ClearClip);
+                Debug.Log("이겼어요");
                 Time.timeScale = 0.0f;
                 Clear.SetActive(true);
             }
         }
         else
         {
+            audioSource.PlayOneShot(FailClip);
+            Debug.Log("졌어요");
             firstCard.CloseCard();
             secondCard.CloseCard();
         }
